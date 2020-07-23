d0721276dc8705d390afec634da23760af917ecd
Antoraro <anton.rozdorozhnuk@gmail.com>
2020-07-23 15:46:32 +0300

    bearer and error messages added

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,23 +3,18 @@ import 'dart:io';
 
 import 'package:KeyKeeperApp/models/activation_requests_model.dart';
 import 'package:KeyKeeperApp/models/vaults_model.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String baseUrl = 'https://sirius.swisschain.io/api';
-  static const List<String> bearers = [
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJzaXJpdXMuc3dpc3NjaGFpbi5pbyIsInRlbmFudC1pZCI6IjkzMzQzMGNiLTMyYjItNGNkNi05M2ZkLWE5MzRmODYzYTFiNSIsInVuaXF1ZV9uYW1lIjoia2V5LWtlZXBlci0xIiwia2V5LWtlZXBlci1pZCI6IjEwMDAxMSIsIm5iZiI6MTU5Mjg4ODE0MywiZXhwIjoxNTk4ODIxMjAwLCJpYXQiOjE1OTI4ODgxNDN9.uR9FSMP_sDzLrz4bHxyWOScKnh9S7hH9UxglNLhuTq0',
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJzaXJpdXMuc3dpc3NjaGFpbi5pbyIsInRlbmFudC1pZCI6IjkzMzQzMGNiLTMyYjItNGNkNi05M2ZkLWE5MzRmODYzYTFiNSIsInVuaXF1ZV9uYW1lIjoia2V5LWtlZXBlci0yIiwia2V5LWtlZXBlci1pZCI6IjEwMDAxMiIsIm5iZiI6MTU5Mjg4ODE0MywiZXhwIjoxNTk4ODIxMjAwLCJpYXQiOjE1OTI4ODgxNDN9.mn9_1p9xtmNJsDIwm2apkLuITH391bn8u_G7ArIeYZU',
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJzaXJpdXMuc3dpc3NjaGFpbi5pbyIsInRlbmFudC1pZCI6IjkzMzQzMGNiLTMyYjItNGNkNi05M2ZkLWE5MzRmODYzYTFiNSIsInVuaXF1ZV9uYW1lIjoia2V5LWtlZXBlci0zIiwia2V5LWtlZXBlci1pZCI6IjEwMDAxNCIsIm5iZiI6MTU5Mjg4ODE0MywiZXhwIjoxNTk4ODIxMjAwLCJpYXQiOjE1OTI4ODgxNDN9.0tLASygrwLAKGUPD0RGcihZq40xHnxXyMYLUDM_jnDg',
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJzaXJpdXMuc3dpc3NjaGFpbi5pbyIsInRlbmFudC1pZCI6IjkzMzQzMGNiLTMyYjItNGNkNi05M2ZkLWE5MzRmODYzYTFiNSIsInVuaXF1ZV9uYW1lIjoia2V5LWtlZXBlci00Iiwia2V5LWtlZXBlci1pZCI6IjEwMDAxNSIsIm5iZiI6MTU5Mjg4ODE0MywiZXhwIjoxNTk4ODIxMjAwLCJpYXQiOjE1OTI4ODgxNDN9.QAw0wTPgHAecOvv85CUesY_PXyoYCs0w1qT8BjgnJa4',
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJzaXJpdXMuc3dpc3NjaGFpbi5pbyIsInRlbmFudC1pZCI6IjkzMzQzMGNiLTMyYjItNGNkNi05M2ZkLWE5MzRmODYzYTFiNSIsInVuaXF1ZV9uYW1lIjoia2V5LWtlZXBlci01Iiwia2V5LWtlZXBlci1pZCI6IjEwMDAxNiIsIm5iZiI6MTU5Mjg4ODE0MywiZXhwIjoxNTk4ODIxMjAwLCJpYXQiOjE1OTI4ODgxNDN9.e3Y_GRVLEfdwG6g5_omTB6YBJvu-cLJai9da6Gy0Vj8',
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJzaXJpdXMuc3dpc3NjaGFpbi5pbyIsInRlbmFudC1pZCI6IjkzMzQzMGNiLTMyYjItNGNkNi05M2ZkLWE5MzRmODYzYTFiNSIsInVuaXF1ZV9uYW1lIjoia2V5LWtlZXBlci02Iiwia2V5LWtlZXBlci1pZCI6IjEwMDAxNyIsIm5iZiI6MTU5Mjg4ODE0MywiZXhwIjoxNTk4ODIxMjAwLCJpYXQiOjE1OTI4ODgxNDN9.g6SkC6Nv',
-  ];
 
   static const String requestsEndpoint = '/vault-activation-requests';
   static const String vaultsEndpoint = '/vaults';
 
-  String get usedBearer => bearers[4];
+  String get usedBearer =>
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsic3dpc3NjaGFpbi5pbyIsInNpcml1cy5zd2lzc2NoYWluLmlvIiwiZXhjaGFuZ2Uuc3dpc3NjaGFpbi5pbyJdLCJ1c2VyLWlkIjoiNTM0ZDAzYmYtM2M4NS00ODA2LWFjYjctZmNjZDRkMzdmODAxIiwidGVuYW50LWlkIjoiNzVmZDY2MWItZDZiNC00MGFjLWFmMWUtNDE0YTc2YzU5OWNlIiwibmJmIjoxNTk1MzExMDk5LCJleHAiOjE1OTU5MTU4OTksImlhdCI6MTU5NTMxMTA5OX0.KlhJdW2VgoxaVTEFLQIa-CIwtcJhbEOmJ2YZYxVR4FE';
 
   Future<VaultsModel> fetchVaults() async {
     final response = await http.get(
@@ -32,8 +27,15 @@ class ApiService {
       print(jsonStr);
       return VaultsModel.fromJson(jsonStr);
     } else {
-      throw Exception('!!!!FAILED to load vaults with code: '
-          '${response.statusCode}');
+      Future.delayed(Duration()).then((_) {
+        Get.defaultDialog(
+          title: 'Vaults Error ${response.statusCode ?? ''}',
+          middleText: response.reasonPhrase ?? 'Something wrong',
+          buttonColor: Colors.black,
+          onConfirm: () => Get.back(),
+        );
+      });
+      return VaultsModel.empty();
     }
   }
 
@@ -48,8 +50,15 @@ class ApiService {
       print(jsonStr);
       return ActivationRequestsModel.fromJson(jsonStr);
     } else {
-      throw Exception('!!!!FAILED to load vaults with code: '
-          '${response.statusCode}');
+      Future.delayed(Duration()).then((_) {
+        Get.defaultDialog(
+          title: 'Activation Requests Error ${response.statusCode ?? ''}',
+          middleText: response.reasonPhrase ?? 'Something wrong',
+          buttonColor: Colors.black,
+          onConfirm: () => Get.back(),
+        );
+      });
+      return ActivationRequestsModel.empty();
     }
   }
 }
