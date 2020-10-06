321f11efdb6960d0d8dd5c0c15284f377f0e6993
anton.rozdorozhniuk <anton.rozdorozhniuk@swisschain.io>
2020-10-06 23:10:49 +0300

    firebase push notifications enabled

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:KeyKeeperApp/app/routes/app_routes.dart';
 import 'package:KeyKeeperApp/app/ui/app_themes.dart';
 import 'package:KeyKeeperApp/bindings/initial_binding.dart';
 import 'package:KeyKeeperApp/ui/pages/root/root_page.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:get/get.dart';
 
 void main() async {
   await GetStorage.init();
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   runApp(
     GestureDetector(
       onTap: () {
@@ -26,7 +28,44 @@ void main() async {
         transitionDuration: const Duration(milliseconds: 150),
         initialRoute: RootPage.route,
         initialBinding: InitialBinding(),
+        onInit: () async {
+          _firebaseMessaging.configure(
+            onMessage: (Map<String, dynamic> message) async {
+              print("onMessage: $message");
+            },
+            onBackgroundMessage: backgroundMessageHandler,
+            onLaunch: (Map<String, dynamic> message) async {
+              print("onLaunch: $message");
+            },
+            onResume: (Map<String, dynamic> message) async {
+              print("onResume: $message");
+            },
+          );
+          await _firebaseMessaging.requestNotificationPermissions(
+            const IosNotificationSettings(
+              sound: true,
+              badge: true,
+              alert: true,
+              provisional: true,
+            ),
+          );
+          _firebaseMessaging.onIosSettingsRegistered.listen(
+            (IosNotificationSettings settings) {
+              print("Settings registered: $settings");
+            },
+          );
+          _firebaseMessaging.getToken().then(
+            (String token) {
+              assert(token != null);
+              print('token $token');
+            },
+          );
+        },
       ),
     ),
   );
+}
+
+Future<dynamic> backgroundMessageHandler(Map<String, dynamic> message) async {
+  print("onBackgroundMessage: $message");
 }
