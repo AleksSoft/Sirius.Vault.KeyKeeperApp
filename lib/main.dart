74288a2badabdc214a45892fa9bc62f937c2cf57
Anton R <anton.rozdorozhnuk@gmail.com>
2020-10-19 16:46:32 +0300

    android gradle updated + default main.dart set to prod env

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'package:validator/app/utils/app_config.dart';
 import 'main_common.dart';
 
 Future<void> main() async {
-  await mainCommon(Environment.dev);
+  await mainCommon(Environment.prod);
 }
