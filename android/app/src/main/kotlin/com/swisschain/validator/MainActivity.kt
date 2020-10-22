8c79d130e87495ddee78b97114d45c9e0e088d42
Anton R <anton.rozdorozhnuk@gmail.com>
2020-10-23 01:31:47 +0300

    android main activity extended from FlutterFragmentActivity to handle touch id

--- android/app/src/main/kotlin/com/swisschain/validator/MainActivity.kt
+++ android/app/src/main/kotlin/com/swisschain/validator/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.swisschain.validator
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
